a025088cd69fdcb364f8b259f3f22e9b8c96d027
sam bacha <sam@freighttrust.com>
2020-08-26 21:25:05 -0700

    Create multi-container.sh

---
+++ entry-point/multi-container.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+# ensure that the ENTRYPOINT script receives the Unix signals, passes them on, and then does some more work:
+
+# USE the trap if you need to also do manual cleanup after the service is stopped,
+#     or need to start multiple services in the one container
+trap "echo TRAPed signal" HUP INT QUIT TERM
+
+# start service in background here
+/usr/sbin/apachectl start
+
+echo "[hit enter key to exit] or run 'docker stop <container>'"
+read
+
+# stop service and clean up here
+echo "stopping apache"
+/usr/sbin/apachectl stop
+
+echo "exited $0"
